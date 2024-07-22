f61a8ad5840ea8b20dc8d2e4e8b62f406456cf33
Matheus Costa <matheuscuisine@gmail.com>
2024-07-22 15:10:31 -0300

    Pequena correção na questão 6

--- Exercícios DICIONÁRIO/Exercício 6.py
+++ Exercícios DICIONÁRIO/Exercício 6.py
@@ -5,7 +5,8 @@ frutas = {
 # Utilizando a classe "str" e adicionando as "chaves" com seus respectivos "valores", utilizando a classe "str", contendo números com a classe "int".
 'maçã': 3, 
 'banana': 5, 
-'uva': 2}
+'uva': 2
+}
 
 # Somando mais dois ao "valor" da entrada com "chave" igual a "maçã" do dicionário "frutas".
 frutas['maçã'] += 2
